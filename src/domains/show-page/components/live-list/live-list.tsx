39f0930b4782406603a478d773ed4e54cc3df134
Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2024-10-19 17:11:30 +0200

    test

--- src/domains/show-page/components/live-list/live-list.tsx
+++ src/domains/show-page/components/live-list/live-list.tsx
@@ -4,45 +4,46 @@ import live2 from '../../../../public/video/live_corner_3.mp4';
 import live3 from '../../../../public/video/live_corner_2.mp4';
 import LiveItem from '../live-item/live-item';
 
-const lives = [
-{
-  location: 'Baobab Bar',
-  city:'Tokyo',
-  cityJp:'東京都',
-  date: 2022,
-  video:live
-},
-{
-  location: 'Corner stone Bar',
-  city:'Osaka',
-  cityJp:'大阪市',
-  date: 2024,
-  video:live2
-},
-{
-  location: 'Corner stone Bar',
-  city:'Osaka',
-  cityJp:'大阪市',
-  date: 2023,
-  video:live3
-},
-{
-  location: 'Corner stone Bar',
-  city:'Osaka',
-  cityJp:'大阪市',
-  date: 2023,
-  video:live3
-},
-{
-  location: 'Corner stone Bar',
-  city:'Osaka',
-  cityJp:'大阪市',
-  date: 2023,
-  video:live3
-}
-]
+
 
 const LiveList = ({locale}:{locale:string}) => {
+  const lives = [
+    {
+      location: 'Baobab Bar',
+      city:'Tokyo',
+      cityJp:'東京都',
+      date: 2022,
+      video:live
+    },
+    {
+      location: 'Corner stone Bar',
+      city:'Osaka',
+      cityJp:'大阪市',
+      date: 2024,
+      video:live2
+    },
+    {
+      location: 'Corner stone Bar',
+      city:'Osaka',
+      cityJp:'大阪市',
+      date: 2023,
+      video:live3
+    },
+    {
+      location: 'Corner stone Bar',
+      city:'Osaka',
+      cityJp:'大阪市',
+      date: 2023,
+      video:live3
+    },
+    {
+      location: 'Corner stone Bar',
+      city:'Osaka',
+      cityJp:'大阪市',
+      date: 2023,
+      video:live3
+    }
+    ]
   return (
     <div className='flex flex-col md:flex-row flex-wrap gap-4 w-full'>
       {lives && lives.sort((a, b)=> b.date - a.date).map((item, index) =>
