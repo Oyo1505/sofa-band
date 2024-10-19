cba51cb4e935157aedcadf3607bbc6fc3c25ff2f
Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2024-10-19 18:20:27 +0200

    test

--- src/domains/show-page/components/live-item/live-item.tsx
+++ src/domains/show-page/components/live-item/live-item.tsx
@@ -2,11 +2,12 @@
 import { Play } from '@/domains/ui/components/icons/icons'
 import Text from '@/domains/ui/components/text/text'
 import Title from '@/domains/ui/components/title/title'
+import moment from 'moment'
 import React, { Suspense, useRef, useState } from 'react'
 
 interface Props {
   location : string
-  date : number
+  date : Date
   video : string
   locale:string
   city:string
@@ -27,7 +28,6 @@ const LiveItem = ({location, video, date, city, cityJp, locale}:Props) => {
     }
   }
   return (
-     <Suspense fallback={<p>Loading video...</p>}>
     <div className='group flex flex-col md:flex-row gap-3 w-full  md:w-96 relative hover:cursor-pointer'>
       <div className="w-full flex justify-center relative md:w-full "> 
           <video ref={refVideo} 
@@ -36,32 +36,21 @@ const LiveItem = ({location, video, date, city, cityJp, locale}:Props) => {
               onMouseLeave={()=>refVideo.current?.removeAttribute("controls")}  
               className='w-full h-64 object-cover rounded-md'>
             <source src={require(`../../../../public/video/${video}`)} type="video/mp4" />
-           
       </video>
-     
       {!isPlaying && 
       <div onClick={handleVideo} className='group z-8 absolute top-0 left-0 w-full h-full rounded-md bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-opacity duration-300'>
         <div className=' w-full h-full'>
           <div className='flex flex-col justify-center h-full  items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
             <div className="text-2xl text-center font-bold mb-2" ><Play className='size-5' /></div>
             <Title type='h2' className='text-lg text-center md:text-base w-48 md:w-64 truncate'>{location} - {locale === 'jp' ? cityJp : city }</Title>
-            <Text text={Number(date).toString()} type='p' className='text-sm md:text-base' />
+            <Text text={moment(date).format('DD/MM/YYYY')} type='p' className='text-sm md:text-base' />
           </div>
         </div>
       </div>
       }
     </div>
-    </div></Suspense>
+    </div>
   )
 }
 
 export default LiveItem
-
-/*  <ReactPlayer 
-      className='w-full h-64 object-cover rounded-md'
-      url={video} 
-      controls={isPlaying}           
-      playing={isPlaying}
-      width="100%" 
-      height="100%" 
-    />  */
