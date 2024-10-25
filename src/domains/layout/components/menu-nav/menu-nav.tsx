ceb6d95d3426b825a88e1487ed9fbc6e73e3e73a
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2024-10-25 16:19:59 +0200

    back-office-init

--- src/domains/layout/components/menu-nav/menu-nav.tsx
+++ src/domains/layout/components/menu-nav/menu-nav.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useTranslations } from 'next-intl'
 import React from 'react'
-import { URL_HOME, URL_LIVE, URL_MUSIC, URL_SHOWS } from '@/libs/routes'
+import { URL_HOME, URL_LIVE, URL_MUSIC, URL_SHOWS, URL_SIGNIN } from '@/libs/routes'
 import ButtonSwitchLangage from '../button-switch-langage/button-switch-langage'
 import { DiscogsIcon, InstagramIcon, Spotify } from '@/domains/ui/components/icons/icons'
 import { Link } from '@/i18n/routing'
@@ -17,6 +17,7 @@ const MenuNav = ({locale}:{locale:string}) => {
       <div><Link href={URL_LIVE} >{t('Live')}</Link></div>
     </nav>
     <div className='flex gap-4'>
+       <div><Link href={URL_SIGNIN} >{t('Signin')}</Link></div>
       <a target="_blank" href="https://www.instagram.com/sofa_rockers_posse/"><InstagramIcon /></a>
       <a target="_blank" href="https://www.discogs.com/fr/artist/14308751-Sofa-Rockers"><DiscogsIcon /></a>
       <a target="_blank" href="http://cornerstone.shop-pro.jp/?pid=176992470"><Spotify /></a>
