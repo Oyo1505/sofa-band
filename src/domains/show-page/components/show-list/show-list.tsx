bd65faeadb68a76ca08f6b08777cf3df18dedbf4
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2025-04-15 13:32:13 +0200

    tes

--- src/domains/show-page/components/show-list/show-list.tsx
+++ src/domains/show-page/components/show-list/show-list.tsx
@@ -5,7 +5,8 @@ import { headers } from 'next/headers'
 
 const ShowList = async ({events}: {events: Event[]}) => {
    const userAgent = (await headers()).get('user-agent');
-   const isChromeBrowser = userAgent?.includes('Chrome')
+   const isChromeBrowser = userAgent?.includes('Chrome') || false
+
   return (
     <div className='rounded-2xl w-full'>
    {events?.map((event: Event, index: number) => (
