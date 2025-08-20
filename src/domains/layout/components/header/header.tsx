3a3ffaa5b3ed27151e8f7244711225945fe9459c
Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2025-08-20 12:03:30 +0200

    header-ok

--- src/domains/layout/components/header/header.tsx
+++ src/domains/layout/components/header/header.tsx
@@ -1,15 +1,22 @@
 'use client'
 import Container from '@/domains/ui/components/container/container'
-import React, { Suspense } from 'react'
+import React, { Suspense, useRef } from 'react'
 import MenuNav from '../menu-nav/menu-nav'
 import dynamic from 'next/dynamic';
+import useScrollPosition from '../../hooks/use-scroll-positon';
+import { useDimensions } from '../../hooks/use-dimensions';
 
 const MenuMobile = dynamic(() => import('../menu-mobile/menu-mobile'), { ssr: false });
 
 const Header = ({ locale }: { locale: string }) => {
+  const positionY = useScrollPosition()
+  const ref = useRef<HTMLDivElement>(null)
+  const {height} = useDimensions(ref)
+  const isScrolledAfterHeader = positionY > height
+
   return (
     <>
-      <header className={` font-bold hidden md:flex group fixed w-full top-0  h-15  pt-2 pb-2 items-center gap-4 z-20 justify-between lg:justify-end`}>
+      <header ref={ref} className={`font-bold hidden md:flex group fixed w-full top-0  h-15  pt-2 pb-2 items-center gap-4 z-20 justify-between lg:justify-end transition-all duration-300 bg-neutral-900 ${isScrolledAfterHeader ? 'shadow-md shadow-neutral-800/50' : 'shadow-none'}`}>
         <Container >
           <MenuNav locale={locale} />
         </Container>
