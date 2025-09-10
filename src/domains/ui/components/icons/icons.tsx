d57ed02938ec369837947041b6efafea9a081ffc
Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2025-09-10 11:33:36 +0200

    displayName =LanguageLogo

--- src/domains/ui/components/icons/icons.tsx
+++ src/domains/ui/components/icons/icons.tsx
@@ -128,7 +128,7 @@ export const LanguageLogo = memo(() => {
   )
 
 })
-
+LanguageLogo.displayName = 'LanguageLogo'
 export const Prev = memo(function Prev(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg  {...props} className="h-8 w-8 text-neutral-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <polygon points="19 20 9 12 19 4 19 20" />  <line x1="5" y1="19" x2="5" y2="5" /></svg>
